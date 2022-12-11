3a018fff31f5ef25e4299b819ec3bd9a996b9d2d
Ido Bronfeld <idobronfeld@gmail.com>
2022-12-11 23:42:24 +0200

    Added Day6 part1

---
+++ Day6/part1/solve.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def get_preamble_index(buff, preamble_size=4):
+    window = deque([], maxlen=preamble_size)
+
+    for i, _chr in enumerate(buff):
+        if len(set(window)) == preamble_size:
+            return i
+        window.append(_chr)
+
+with open('input.txt', 'r') as fd:
+    buff = fd.read()
+
+print(get_preamble_index(buff))
